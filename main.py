fb266946fe678eaad540a04cd12648be42fdca35
mjyoo2 <mjyoo2@g.skku.edu>
2021-01-13 23:27:03 +0900

    update

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
+import warnings
+warnings.filterwarnings('ignore')
 from stable_baselines import PPO2
 from stable_baselines.common.policies import MlpPolicy
-import gym_nav2d
+from multiprocessing import Process
 import gym
+from reptile_callback import LowCallback, reptile
 
+ALPHA = 0.05
+env_setting = lambda: gym.make('CartPole-v0')
+model_setting = lambda env: PPO2(MlpPolicy, env)
+path = './model'
 
-if __name__ == "__main__":
-    env = gym.make('nav2dmdpgoal-v0')
-    env.goal_setting([127, 127])
-    model = PPO2(MlpPolicy, env, n_steps=1024, verbose=1, tensorboard_log='./', full_tensorboard_log=True)
-    model.learn(1000000, log_interval=100)
+def run(oper_num):
+    env = env_setting()
+    model = model_setting(env)
+    callback = LowCallback(oper_num)
+    model.learn(total_timesteps=300000, callback=callback)
+    print('finish')
+
+def reptile_run(num_oper):
+    env = env_setting()
+    model = model_setting(env)
+    algo = reptile(num_of_operator=num_oper, alpha=ALPHA, model=model, env=env)
+    algo.run()
+    algo.save(path)
+    print('finish')
+    algo.test()
+
+if __name__ == '__main__':
+    p_list = []
+    for i in range(4):
+        p = Process(target=run, args=(i, ))
+        p.start()
+        p_list.append(p)
+        print('make process')
+    p = Process(target=reptile_run, args=(4, ))
+    p.start()
+    p_list.append(p)
+    print('make reptile')
+    for proc in p_list:
+        proc.join()
--- reptile_callback.py
+++ reptile_callback.py
@@ -35,6 +35,9 @@ class LowCallback(BaseCallback):
         self.sock = ctx.socket(zmq.REQ)
         self.sock.connect('tcp://localhost:{}'.format(23232))
         self.request_msg = {'operator_number': oper_num, 'description': 'request'}
+        self.recv_sock = ctx.socket(zmq.SUB)
+        self.recv_sock.connect('tcp://localhost:{}'.format(32323))
+        self.recv_sock.setsockopt_string(zmq.SUBSCRIBE, '')
 
     def _on_rollout_end(self) -> None:
         """
@@ -44,88 +47,72 @@ class LowCallback(BaseCallback):
         msg = {'operator_number': self.request_msg['operator_number'], 'description': 'parameters',
                'parameters': model_parameter}
         self.sock.send(pkl.dumps(msg))
-        res = pkl.dumps(self.sock.recv())
-        while True:
-            self.sock.send(pkl.dumps(self.request_msg))
-            res = pkl.dumps(self.sock.recv())
-            if res['description'] == 'parameters':
-                model_parameter = res['parameters']
-                self.model.load_parameters(model_parameter)
-                break
-            time.sleep(0.1)
+        res = pkl.loads(self.sock.recv())
+
+        res = pkl.loads(self.recv_sock.recv())
+        if res['description'] == 'parameters':
+            model_parameter = res['parameters']
+            self.model.load_parameters(model_parameter)
 
     def _on_training_end(self) -> None:
         """
         This event is triggered before exiting the `learn()` method.
         """
-        pass
+        msg = {'operator_number': self.request_msg['operator_number'], 'description': 'finish'}
+        self.sock.send(pkl.dumps(msg))
 
-class HighCallback(BaseCallback):
-    """
-    A custom callback that derives from ``BaseCallback``.
-
-    :param verbose: (int) Verbosity level 0: not output 1: info 2: debug
-    """
-    def __init__(self, num_of_operator, alpha, verbose=0):
-        super(HighCallback, self).__init__(verbose)
-        # Those variables will be accessible in the callback
-        # (they are defined in the base class)
-        # The RL model
-        # self.model = None  # type: BaseRLModel
-        # An alias for self.model.get_env(), the environment used for training
-        # self.training_env = None  # type: Union[gym.Env, VecEnv, None]
-        # Number of time the callback was called
-        # self.n_calls = 0  # type: int
-        # self.num_timesteps = 0  # type: int
-        # local and global variables
-        # self.locals = None  # type: Dict[str, Any]
-        # self.globals = None  # type: Dict[str, Any]
-        # The logger object, used to report things in the terminal
-        # self.logger = None  # type: logger.Logger
-        # Sometimes, for event callback, it is useful
-        # to have access to the parent object
-        # self.parent = None  # type: Optional[BaseCallback]
+class reptile(object):
+    def __init__(self, num_of_operator, alpha, env, model):
         ctx = zmq.Context()
         self.sock = ctx.socket(zmq.REP)
         self.sock.bind('tcp://*:{}'.format(23232))
+        self.send_sock = ctx.socket(zmq.PUB)
+        self.send_sock.bind('tcp://*:{}'.format(32323))
         self.num_of_operator = num_of_operator
         self.response_msg = {'description': 'response'}
         self.alpha = alpha
+        self.model = model
+        self.env = env
 
-    def _on_rollout_end(self) -> None:
-        """
-        This event is triggered before updating the policy.
-        """
-        num_data = 0
-        data = dict()
-        while num_data != self.num_of_operator:
-            req = pkl.loads(self.sock.recv())
-            if req['description'] == 'parameters':
-                data[str(req['operator_number'])] = req
-                num_data += 1
-            self.sock.send(pkl.dumps(self.response_msg))
+    def run(self):
+        while True:
+            num_data = 0
+            data = dict()
+            while num_data != self.num_of_operator:
+                req = pkl.loads(self.sock.recv())
+                if req['description'] == 'parameters':
+                    data[str(req['operator_number'])] = req
+                    num_data += 1
+                if req['description'] == 'finish':
+                    return
+                self.sock.send(pkl.dumps(self.response_msg))
 
-        # merge parameter
-        model_parameter = self.model.get_parameters()
-        for layer in model_parameter.keys():
-            layer_param = []
-            for i in range(self.num_of_operator):
-                layer_param.append(data[str(i)]['model_parameter'][layer])
-            delta = np.average(layer_param, axis=0)
+            # merge parameter
+            model_parameter = self.model.get_parameters()
+            for layer in model_parameter.keys():
+                layer_param = []
+                for i in range(self.num_of_operator):
+                    layer_param.append(data[str(i)]['parameters'][layer])
+                delta = np.average(layer_param, axis=0)
 
-            model_parameter[layer] = (1 - self.alpha) * model_parameter[layer] + self.alpha * delta
+                model_parameter[layer] = (1 - self.alpha) * model_parameter[layer] + self.alpha * delta
+            self.model.load_parameters(model_parameter)
+            model_parameter = self.model.get_parameters()
 
-        model_parameter = self.model.get_parameters()
-        operator_list = list(np.arange(self.num_of_operator) + 1)
-        while operator_list:
-            req = pkl.loads(self.sock.recv())
-            if req['operator_number'] in operator_list:
-                msg = {'description': 'parameters', 'parameters': model_parameter}
-                self.sock.send(msg)
-                operator_list.remove(req['operator_number'])
+            msg = {'description': 'parameters', 'parameters': model_parameter}
+            self.send_sock.send(pkl.dumps(msg))
 
-    def _on_training_end(self) -> None:
-        """
-        This event is triggered before exiting the `learn()` method.
-        """
-        pass
+    def test(self):
+        sum_of_reward = 0
+        for i in range(10):
+            done = False
+            state = self.env.reset()
+            while not done:
+                action, _ = self.model.predict(state)
+                state, reward, done, info = self.env.step(action)
+                sum_of_reward += reward
+        print(sum_of_reward/10)
+        return
+
+    def save(self, path):
+        self.model.save(path)
